e5472296f0886fe8196690ae830b974f5844bc1e
alifaizyab8 <alifaizyab8@gmail.com>
2025-12-30 17:08:47 +0500

    Brute Force Method Added

---
+++ Challenge 1/teamContributionMultiplier1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> func(vector<int> &arr)
+{
+    int multiplier;
+    int s = arr.size();
+    vector<int> contribution(s);
+    for (int i = 0; i < s; i++)
+    {
+        multiplier = 1;
+        for (int j = 0; j < s; j++)
+        {
+            if (arr[i] == arr[j])
+            {
+                continue;
+            }
+            else
+            {
+                multiplier *= arr[j];
+            }
+        }
+        contribution[i] = multiplier;
+    }
+    return contribution;
+}
+int main()
+{
+    vector<int> arr1 = {-1, 1, 0, -3, 3};
+    vector<int> arr2 = func(arr1);
+    for (int x : arr2)
+    {
+        cout << " " << x;
+    }
+}
